e5cc9742d2a16d556735c6a1adc07634e247c2d0
Brand TIKOUE TIKOUE <93736627+brandgit@users.noreply.github.com>
2022-11-16 18:08:17 +0100

    Commit de la merde que tu es

--- appevents/src/pages/Home.jsx
+++ appevents/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <div>
-        <h1 className='font-bold text-green-400'>Home</h1>
+        <h1 className='font-bold text-green-400'>Amine tu es une merde</h1>
     </div>
   )
 }
