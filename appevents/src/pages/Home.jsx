5c7dead3d2266b2a7fd031070f0646039a62def1
Brand TIKOUE TIKOUE <93736627+brandgit@users.noreply.github.com>
2022-11-16 17:15:41 +0100

    My first commit

--- appevents/src/pages/Home.jsx
+++ appevents/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+        <h1 className='font-bold text-green-400'>Home</h1>
+    </div>
   )
 }
