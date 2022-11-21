2b6b256411a42ab22a627a962f8af5cacf50ffd5
Brand TIKOUE TIKOUE <93736627+brandgit@users.noreply.github.com>
2022-11-21 14:08:30 +0100

    Commit du 21/11/2022 à 14:08

--- appevents/src/components/Header/Header.jsx
+++ appevents/src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ import SearchBar from './SearchBar'
 export default function Header() {
   return (
     <div className='flex w-full h-[75px] justify-center  bg-blue-900'>
-          <div className='flex justify-between w-[95%] h-[100%] bg-red-900'>
+          <div className='flex justify-between w-[100%] h-[100%] bg-red-900'>
               <div className='wrapperLogoMenuBurger' >
                     <MenuBurger/>
                     <Logo/>
--- appevents/src/components/Header/SearchBar.jsx
+++ appevents/src/components/Header/SearchBar.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 export default function SearchBar() {
   return (
     <div className='wrapperSearchBar' >
+        <div className='flex w-full h-full justify-center items-center'>
+            <div className='w-[90%] h-[40px] bg-[grey]'>
 
+            </div>
+        </div>
     </div>
   )
 }
