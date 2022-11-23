9ff19d72eeca1bb3c9208ec4f8fe06756145fd11
AmineHMZ27 <amine.hamzaoui@efrei.net>
2022-11-23 13:39:28 +0100

    Commit pour Betsalel

--- appevents/src/pages/SignIn.jsx
+++ appevents/src/pages/SignIn.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 export default function SignIn() {
   return (
     <div>
+<div className='flex items-center justify-center w-full h-[100vh] bg-black  '>
       <h1 className='text-green-400 font-bold'>Brand le chauve</h1>
+      </div>
+      
     </div>
   )
 }
