531c72f46a218fef7f6a5587fa3f890a2921ebfc
Brand TIKOUE TIKOUE <93736627+brandgit@users.noreply.github.com>
2022-12-06 13:45:05 +0100

    Commit du 06/12/2022 à 13:45

---
+++ appevents/src/assets/data/data.js
@@ -0,0 +1,38 @@
+
+export const events = [
+    {
+        id:"0",
+        title:"Tayc en concert",
+        cover: require("../images/SingIn-bg.jpg"),
+    },
+    {
+        id:"1",
+        title:"Wizkid en concert",
+        cover: require("../images/SingIn-bg.jpg"),
+    },
+    {
+        id:"2",
+        title:"Festival de Can",
+        cover: require("../images/SingIn-bg.jpg"),
+    },
+    {
+        id:"3",
+        title:"Festival",
+        cover: require("../images/SingIn-bg.jpg"),
+    },
+    {
+        id:"4",
+        title:"Spectacle1",
+        cover: require("../images/SingIn-bg.jpg"),
+    },
+    {
+        id:"5",
+        title:"Spectacle2",
+        cover: require("../images/SingIn-bg.jpg"),
+    },
+    {
+        id:"6",
+        title:"Spectacle3",
+        cover: require("../images/SingIn-bg.jpg"),
+    },
+]
