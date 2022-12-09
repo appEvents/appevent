3eed3d6d21092f8f3afee1a7dd692f323faaa3b1
AmineHMZ27 <amine.hamzaoui@efrei.net>
2022-12-09 12:08:46 +0100

    Commit 09/12/22 12:09

--- appevents/src/pages/SignIn.jsx
+++ appevents/src/pages/SignIn.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import Logo from '../components/Goba/Logo'
-import Header from '../components/Header/Header'
+import { Link } from "react-router-dom";
+
+function Clear() {
+  // eslint-disable-next-line no-restricted-globals
+  location.reload();
+}
 
 export default function SignIn() {
   return (
@@ -8,17 +13,16 @@ export default function SignIn() {
         <div className='flex items-center justify-center w-full h-[100vh]  '>
           <div  className='pop-up'>
             <div className='px-7 flex items-center w-full h-[70px]  bg-transparent '>
-              <Logo />
+            <Link to="/*"><Logo /></Link>
               <div className='flex justify-evenly items-center w-[250px] h-[100%] bg-transparent '>
-              <h1 >Home</h1>
-              <h1 >Join</h1>
+              <Link to="/*"><h1 >Home</h1></Link>
               </div>
             </div>
-            <div className='flex justify-start px-12 border-box w-[70%] h-[87%]'> 
+            <div className='flex justify-start px-12 border-box w-[100%] h-[87%]'> 
               <div className='createaccount'>
                 <h3>START FOR FREE</h3>
                 <h1>Create new account</h1>
-                <h2>Already member ? <span className='text-[#009640]'>Log In</span></h2>
+                <h2>Already member ? <Link to="/signUp"><span className='text-[#009640]'>Log In</span></Link></h2>
                 <div className='input'>
                   <input placeholder="First name" type="text" />
                   <input placeholder="Last name" type="text" />
@@ -28,7 +32,7 @@ export default function SignIn() {
                   <input type="password" id="pass" name="password" placeholder="Password" minlength="8" required />
                 </div>
                 <div className='Btn'>
-                  <button className='bg-[#3b3a3a]'>Clean</button>
+                  <button onClick={Clear} className='bg-[#3b3a3a]'>Clean</button>
                   <button className='bg-[#009640]'>Create account</button>
                 </div>
               </div>
--- appevents/src/pages/SignUp.jsx
+++ appevents/src/pages/SignUp.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import Logo from '../components/Goba/Logo'
+import { Link } from "react-router-dom";
+
+function Clear() {
+  // eslint-disable-next-line no-restricted-globals
+  location.reload();
+}
 
 export default function SignUp() {
   return (
-    <div className='flex items-center justify-center w-full h-[100vh] bg-black  '>
-      <h1 className='text-green-400 font-bold'>Bienvenue</h1>
+    <div>
+        <div className='flex items-center justify-center w-full h-[100vh]  '>
+          <div  className='pop-up'>
+            <div className='px-7 flex items-center w-full h-[70px]  bg-transparent '>
+            <Link to="/*"><Logo /></Link>
+              <div className='flex justify-evenly items-center w-[250px] h-[100%] bg-transparent '>
+              <Link to="/*"><h1 >Home</h1></Link>
+              </div>
+            </div>
+            <div className='flex justify-start px-12 border-box w-[70%] h-[87%]'> 
+              <div className='createaccount'>
+                <h3>LET'S SEE WHAT'S NEW</h3>
+                <h1>Welcome back</h1>
+                <h2>Not a member yet ? <Link to="/SignIn"><span className='text-[#009640]'>Sign In for free</span></Link></h2>
+                <div className='longer'>
+                  <input type="email" placeholder="Email" />
+                  <input type="password" id="pass" name="password" placeholder="Password" minlength="8" required />
+                </div>
+                <div className='Btn'>
+                  <input onClick={Clear} className='bg-[#3b3a3a]' type="reset" value="Clean"/>
+                  <button className='bg-[#009640]'>Create account</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
   )
 }
